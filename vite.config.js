5fd86c401390467ef0d61edd6cde70c9c6e1b5c6
Young <allen@gmail.com>
2021-07-04 19:58:05 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import WindiCSS from 'vite-plugin-windicss'
 
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Pages(), WindiCSS()],
   resolve: {
@@ -17,6 +16,41 @@ export default defineConfig({
       '@pages': resolve(__dirname, './src/pages'),
       '@store': resolve(__dirname, './src/store'),
       '@utils': resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    https: false,
+    port: 8083,
+    compress: true,
+    open: false, // opens browser window automatically
+    proxy: {
+      // with options
+      '/api': {
+        target: 'http://localhost:8090',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    brotliSize: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('/node_modules/')) {
+            //设置需要独立打包的npm包
+            const modules = ['windicss']
+            const chunk = modules.find((module) => id.includes(`/node_modules/${module}`))
+            return chunk ? `vendor-${chunk}` : 'vendor'
+          }
+        },
+      },
     },
   },
 })
